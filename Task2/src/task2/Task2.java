81d41f0be57da490b5422ecf3eb3009ddbec5da0
Fatima Mujahid <fatymaah31@gmail.com>
2021-04-25 14:46:55 +0500

    Task 2
    Complete

--- Task2/src/task2/Task2.java
+++ Task2/src/task2/Task2.java
@@ -5,18 +5,36 @@
  */
 package task2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author fatym
  */
 public class Task2 {
+    
+    public static void displayVertical(String str) 
+    {
+        char line = '\0';
+        String data1 = str.replaceAll("\\s", "");
+        int size = data1.length();
+        for(int i = 0;i<size;i++)
+        {
+            line = data1.charAt(i);
+            System.out.println(line);
+        }
+    }
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+        Scanner object = new Scanner(System.in);
+        String data = "";
+        System.out.println("Please enter a Sentence");
+        data = object.nextLine();
+        displayVertical(data); 
     }
     
 }
